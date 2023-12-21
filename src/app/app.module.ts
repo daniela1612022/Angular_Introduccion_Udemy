12d9ca244151610e18c2e22bde70720f980baf80
Daniela Pinzon <danispc389@gmail.com>
2023-12-20 19:56:38 -0500

    Integracion de modulos heroes y counter

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,21 +1,21 @@
 import { NgModule } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
 
+import { HeroesModule } from './heroes/heroes.module';
+import { BrowserModule } from '@angular/platform-browser';
+import { CounterModule } from './counter/components/counter.module';
 import { AppRoutingModule } from './app-routing.module';
+
 import { AppComponent } from './app.component';
-import { CounterComponent } from './counter/counter.component';
-import { HeroComponent } from './heroes/hero/hero.component';
-import { ListComponent } from './heroes/list/list.component';
+
 
 @NgModule({
   declarations: [
-    AppComponent,
-    CounterComponent,
-    HeroComponent,
-    ListComponent
+    AppComponent
   ],
   imports: [
     BrowserModule,
+    CounterModule,
+    HeroesModule,
     AppRoutingModule
   ],
   providers: [],
