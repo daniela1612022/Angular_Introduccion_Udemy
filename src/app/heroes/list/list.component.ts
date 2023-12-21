0ea07e43537cbeb0e7d695224e0470d6126bebcd
Daniela Pinzon <danispc389@gmail.com>
2023-12-20 19:16:52 -0500

    Lista de Heroes con ng if - else -for

--- src/app/heroes/list/list.component.ts
+++ src/app/heroes/list/list.component.ts
@@ -7,4 +7,11 @@ import { Component } from '@angular/core';
 })
 export class ListComponent {
 
+  public heroesNames : string[] = ['Spiderman' , 'Iron Man' , 'Hulk' , 'Capitan America' , 'Thor' , 'Dr Strange'];
+  public deletedHero? : string ;
+
+  // Metodo para borrar ultimo elemento del array
+  removeLastHero():void{
+    this.deletedHero = this.heroesNames.pop();
+  }
 }
